838747f76fd07fac936f5624b8819550c53e5c83
Vitalii Khorobchuk <vkhorobchuk@gmail.com>
2022-01-23 01:36:45 +0200

    Architecture structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
             isDebuggable = true
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
