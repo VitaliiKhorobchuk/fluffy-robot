bd741241b4b4b2bbf581badf67691b6cd449af48
Vitalii Khorobchuk <vkhorobchuk@gmail.com>
2022-01-30 18:53:57 +0200

    Added home screen design, created Document details screen

--- app/src/main/kotlin/com/sixoutoften/recepier/ui/home/HomeActivity.kt
+++ app/src/main/kotlin/com/sixoutoften/recepier/ui/home/HomeActivity.kt
@@ -2,24 +2,36 @@ package com.sixoutoften.recepier.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sixoutoften.recepier.MainNavigationDirections
 import com.sixoutoften.recepier.R
+import com.sixoutoften.recepier.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        val bnv = findViewById<BottomNavigationView>(R.id.bnvHome)
-        bnv.setupWithNavController(Navigation.findNavController(this, R.id.fcvHome))
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, DocumentListFragment())
-//                .commitNow()
-//        }
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fcvHome)
+        binding.bnvHome.setupWithNavController(navController)
+
+        binding.fabNewDocument.let { fab ->
+            fab.setOnClickListener {
+                navController.navigate(MainNavigationDirections.navigateToDocumentDetail())
+            }
+            // TODO: connect to list scroll or empty state
+//            if (fab.isExtended) {
+//                fab.shrink()
+//            } else {
+//                fab.extend()
+//            }
+        }
     }
 }
---
+++ app/src/main/kotlin/com/sixoutoften/recepier/ui/home/document_detail/DocumentDetailActivity.kt
@@ -0,0 +1,19 @@
+﻿package com.sixoutoften.recepier.ui.home.document_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.sixoutoften.recepier.databinding.ActivityDocumentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DocumentDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDocumentDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDocumentDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
